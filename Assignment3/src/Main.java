7d4f78a79be1995de6e1ca2f94b95c51ca9af95e
AlejandraS15 <141249376+AlejandraS15@users.noreply.github.com>
2024-05-10 08:14:49 -0500

    Se añadio metodo nombre

--- Assignment3/src/Main.java
+++ Assignment3/src/Main.java
@@ -1,7 +1,10 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void nombre(){
+        System.out.println("Assignment 3 by Mayerly Alejandra Suarez Sepulveda");
+    }
     public static void main(String[] args) {
-
+        nombre();
     }
 }
